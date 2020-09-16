fabd3f2b7c16aac27f1e7a10a3a018bf4904349f
Rick Dawson <rick.dawson@blackhorsesolutions.com>
2020-09-16 15:07:57 -0400

    Initial dev branch commit

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ git clone git://github.com/radawson/py-kms
 cd py-kms
 sh install.sh
 
-# TODO: Add https redirect
+# Pull openssl modification 
+HTTP_MOD=`curl https://raw.githubusercontent.com/rdbh/openwrt-config/master/config.txt`
+echo $HTTP_MOD >> /etc/lighttpd/conf.d/30-openssl.conf
+
+ # Restart the service
+ /etc/init.d/lighttpd restart
