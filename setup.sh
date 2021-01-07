429ef297939ce8f89e8274107a9af8b6b7f3b820
Rick Dawson <dawsora@gmail.com>
2021-01-07 12:56:33 -0500

    Update setup.sh
    version bump to 0.2.4 
    include packages for usb filesharing
    include python packages to make python3-light support py-kms

--- setup.sh
+++ setup.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 # Script for setting up a new OpenWRT device
-# Copyright 2020 R Dawson
-# v0.2.3
+# Copyright 2020, 2021 R Dawson
+# v0.2.4
 
 # Exit on errors
 set -e
 
 printf "\nStep 1 - Updating Repository\n\n"
 opkg update
+# (0.2.3) uncomment the following line to update all packages
+# this is equivalent to apt upgrade
 # opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade 
 printf "\nStep 2 - software installation\n"
 printf "\n\tStep 2a - Installing tmux\n"
 opkg install tmux
 printf "\n\tStep 2b - Installing python 3.x\n"
 opkg install python3-light
-# following additional libraries are required for py-kms
+# (0.2.4) following additional libraries are required for py-kms 
 opkg install python3-logging
 opkg install python3-xml
 opkg install python3-multiprocessing
 printf "\n\tStep 2c - Installing git\n"
 opkg install git
-# 0.2.2 removed nano to save space and simplify install
-# printf "Step 2d - Installing nano"
+# (0.2.4) added file drivers to make USB sharing easier
+printf "\n\tStep 2d - Installing drivers for file sharing\n"
+opkg install e2fsprogs
+opkg install kmod-usb3
+# (0.2.2) removed nano to save space and simplify install
+# printf "Step 2e - Installing nano"
 # opkg install nano
 
 printf "\nStep 3 - Installing py-kms\n\n"
